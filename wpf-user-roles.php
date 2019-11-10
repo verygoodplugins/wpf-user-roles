4375c052ca2ea9d91a902803757f2477f8023f09
Jack <info@verygoodplugins.com>
2019-11-10 09:23:50 +0100

    Modified so roles can also be removed

--- wpf-user-roles.php
+++ wpf-user-roles.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 /*
 Plugin Name: WP Fusion - User Roles
@@ -12,7 +12,7 @@ Author URI: https://verygoodplugins.com/
 function wpf_user_roles_tags_modified( $user_id, $user_tags ) {
 
 	// Don't run for admins
-	if( user_can( $user_id, 'manage_options' ) ) {
+	if ( user_can( $user_id, 'manage_options' ) ) {
 		return;
 	}
 
@@ -20,25 +20,29 @@ function wpf_user_roles_tags_modified( $user_id, $user_tags ) {
 
 	$user = get_user_by( 'id', $user_id );
 
-	foreach( $settings as $role_slug => $setting ) {
+	remove_action( 'profile_update', array( wp_fusion()->user, 'profile_update' ), 10, 2 );
 
-		if( empty( $setting['tag_link'] ) ) {
+	foreach ( $settings as $role_slug => $setting ) {
+
+		if ( empty( $setting['tag_link'] ) ) {
 			continue;
 		}
 
-		$result = array_intersect($user_tags, $setting['tag_link']);
+		$result = array_intersect( $user_tags, $setting['tag_link'] );
 
-		if( ! empty( $result ) && ! in_array($role_slug, $user->roles) ) {
+		if ( ! empty( $result ) && ! in_array( $role_slug, $user->roles ) ) {
 
-			wp_fusion()->logger->handle( 'info', $user_id, 'Changing user role to <strong>' . $role_slug . '</strong> from linked tag <strong>' . $setting['tag_link'][0] . '</strong>' );
+			wp_fusion()->logger->handle( 'info', $user_id, 'Adding user role <strong>' . $role_slug . '</strong>, triggered by linked tag <strong>' . $setting['tag_link'][0] . '</strong>' );
 
-			remove_action( 'profile_update', array( wp_fusion()->user, 'profile_update'), 10, 2);
+			$user->add_role( $role_slug );
 
-			wp_update_user( array( 'ID' => $user_id, 'role' => $role_slug ) );
-			return;
+		} elseif ( empty( $result ) && in_array( $role_slug, $user->roles ) ) {
+
+			wp_fusion()->logger->handle( 'info', $user_id, 'Removing user role <strong>' . $role_slug . '</strong>, triggered by linked tag <strong>' . $setting['tag_link'][0] . '</strong>' );
+
+			$user->remove_role( $role_slug );
 
 		}
-
 	}
 
 }
@@ -60,7 +64,7 @@ function wpf_user_roles_admin_menu() {
 
 }
 
-add_action( 'admin_menu', 'wpf_user_roles_admin_menu');
+add_action( 'admin_menu', 'wpf_user_roles_admin_menu' );
 
 function wpf_user_roles_enqueue_scripts() {
 
@@ -70,59 +74,60 @@ function wpf_user_roles_enqueue_scripts() {
 
 }
 
-function wpf_user_roles_render_admin_menu() { 
+function wpf_user_roles_render_admin_menu() {
 
 	// Save settings
-
 	if ( isset( $_POST['wpf_roles_settings_nonce'] ) && wp_verify_nonce( $_POST['wpf_roles_settings_nonce'], 'wpf_roles_settings' ) ) {
 		update_option( 'wpf_roles_settings', $_POST['wpf-settings'] );
 		echo '<div id="message" class="updated fade"><p><strong>Settings saved.</strong></p></div>';
-	}			
+	}
 
 	?>
 
-    <div class="wrap">
-        <h2>WP Fusion Roles</h2>
+	<div class="wrap">
+		<h2>WP Fusion Roles</h2>
 
-        <form id="wpf-roles-settings" action="" method="post">
+		<form id="wpf-roles-settings" action="" method="post">
 			<?php wp_nonce_field( 'wpf_roles_settings', 'wpf_roles_settings_nonce' ); ?>
-            <input type="hidden" name="action" value="update">
+			<input type="hidden" name="action" value="update">
 
 
-            <?php global $wp_roles; ?>
+			<?php global $wp_roles; ?>
 
-    		<?php $all_roles = $wp_roles->roles; ?>
-    		<?php $editable_roles = apply_filters('editable_roles', $all_roles); ?>
+			<?php $all_roles      = $wp_roles->roles; ?>
+			<?php $editable_roles = apply_filters( 'editable_roles', $all_roles ); ?>
 
-			<?php $settings = get_option( 'wpf_roles_settings', array() ); ?>
+			<?php $settings       = get_option( 'wpf_roles_settings', array() ); ?>
 
-            <table class="table table-hover" id="wpf-mm-products-table">
-                <thead>
-                <tr>
-                    <th>Role</th>
-                    <th>Link With Tag</th>
-                </tr>
-                </thead>
-                <tbody>
+			<table class="table table-hover" id="wpf-mm-products-table">
+				<thead>
+				<tr>
+					<th>Role</th>
+					<th>Link With Tag</th>
+				</tr>
+				</thead>
+				<tbody>
 
 				<?php foreach ( $editable_roles as $slug => $role ) : ?>
 
-					<?php if ( ! isset( $settings[$slug]['tag_link'] ) ) {
-						$settings[$slug]['tag_link'] = array();
-					} ?>
+					<?php
+					if ( ! isset( $settings[ $slug ]['tag_link'] ) ) {
+						$settings[ $slug ]['tag_link'] = array();
+					}
+					?>
 
-                    <tr>
-                        <td><?php echo $role['name']; ?></td>
-                        <td id="wpf-tags-td">
-                        	<?php 
+					<tr>
+						<td><?php echo $role['name']; ?></td>
+						<td id="wpf-tags-td">
+							<?php
 
 							$args = array(
-								'setting' 		=> $settings[$slug],
-								'meta_name'		=> 'wpf-settings',
-								'field_id'		=> $slug,
-								'field_sub_id'	=> 'tag_link',
-								'placeholder'	=> 'Select a tag',
-								'limit'			=> 1
+								'setting'      => $settings[ $slug ],
+								'meta_name'    => 'wpf-settings',
+								'field_id'     => $slug,
+								'field_sub_id' => 'tag_link',
+								'placeholder'  => 'Select a tag',
+								'limit'        => 1,
 							);
 
 							wpf_render_tag_multiselect( $args );
@@ -130,20 +135,20 @@ function wpf_user_roles_render_admin_menu() {
 							?>
 							
 						</td>
-                    </tr>
+					</tr>
 
 				<?php endforeach; ?>
 
-                </tbody>
+				</tbody>
 
-            </table>
+			</table>
 
-            <p class="submit"><input name="Submit" type="submit" class="button-primary" value="Save Changes"/>
-            </p>
+			<p class="submit"><input name="Submit" type="submit" class="button-primary" value="Save Changes"/>
+			</p>
 
-        </form>
+		</form>
 
-    </div>
+	</div>
 
 	<?php
 
